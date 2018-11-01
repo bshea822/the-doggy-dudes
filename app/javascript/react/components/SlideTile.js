63b8320ed55ee9545f54818121215581b1d0b28f
Brendon Shea <brendonshea@Brendons-MacBook-Air.local>
2018-11-01 06:21:56 -0400

    Changed Carousel on Homepage so it works with Raect

--- app/javascript/react/components/SlideTile.js
+++ app/javascript/react/components/SlideTile.js
@@ -2,11 +2,7 @@ import React from 'react';
 
 const SlideTile = (props) => {
   return(
-    <li className="orbit-slide">
-      <figure className="orbit-figure">
-        <img className="orbit-image" src={props.image} alt="image alt text" />
-      </figure>
-    </li>
+    <img src={props.image} />
   );
 };
  export default SlideTile;
